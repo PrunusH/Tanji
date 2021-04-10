d9599f3fed70ce574e721fa25802ce984e72e68f
PrunusH <54079903+PrunusH@users.noreply.github.com>
2021-04-10 15:24:06 +0200

    * Downgrade
    List of messages that can be generated without find message references:
    Tanji/Habbo/FlashGame.cs -> //FindMessageReferences();
    [Outgoing] Count = 35
    11f82a34fcf7b2b6b67d19fd8f0e3f05 = ?
    13a5b992a88bfdba5d2ed49ca6f5b4dc = ?
    1884d31c69be338c50f2abfcc362fec7 = ?
    214bc7423ee1e2ab0c145628245e49ef = ?
    2bd12fba22fbeabb254e14a9afc23644 = ?
    2f27dc8d133febbbc008ee196d6da53f = ?
    46715723848b48f1cfb7725ab04f6758 = ?
    60517e0a2859eab7395e5161441ce952 = ?
    6714e16bc92bda36bc6c67114e351290 = ?
    68a97de404d9234fc3b22bf73f55b43e = ?
    6a66202d653962ce62310be5f97f6d9e = ?
    792662bf40a4e8961ea0bf4d79e2e89e = ?
    793c2b0ce63c32240f1fbf9928688536 = ?
    7d60c4273068a0b811b83fd36b19824b = ?
    7d67c20625acca74c5a0cfb59538c6df = ?
    892b5840fc79d35aa2d79e8ac3084908 = ?
    9d725c4f13d27d401996e7425df58efd = ?
    a32d7c0fca69a0ce056b1da0c30d373f = ?
    ba847ead6316b92f19b164fc6ccae4da = ?
    bbee455ec980039aab560e48d0e8782e = AcceptFriend
    bc2723d33aec2423658d17bbcab306bb = ?
    be30e915e251986cb08a134c71aa79e0 = ?
    c6eee0570e8d4598680389aa553cb2a9 = ?
    cab5051feefaef39631dcd621b228151 = ?
    cc3f828be381a59cd28eb3c7ced7ff31 = ?
    d35037bc4023835e4785e83d138d8a30 = ?
    d690f230bd7db43ab8ccbaf3afad74c4 = ?
    db6b504dba55a91d60931a8020503944 = ?
    e11bdc0d9182e6d246b6429efef552c7 = ?
    e21d38d8986174cbd67a27ab68806eb3 = ?
    e3e14ba1fbf8b1284b8d34ce93e7d968 = ?
    ea4b7b76ef9dd14d00f45d73105419b9 = ?
    edc9e58a80b39e046d3d5de81cd382e4 = ?
    f325d3a02e7d67617fb774a553fdc123 = ?
    fd3b4c263d094ee41ddb22b44288462a = ?
    [Incoming] Count = 19
    0054ccf0691acbc345597b3f6e9eea92 = ?
    0780cf7233f5f3da8d135d4da6ce05e1 = ?
    0f8d201d8dc9456fe1d805146f44f71a = ?
    1c08f2f54fc08bbe2da13e18417721da = ?
    339671005605c6dc42aca2ed6aac4c46 = ?
    3a32e3cf1aac4718238ae4ced0732f2f = ?
    425ba2164d8189e6eb403ecada8de6f2 = ?
    45411e7b38da5470d6971e50e0281e01 = ?
    65277340d0a35c39660126791c7915b1 = DisconnectReason
    69388218806a22b3f1b4f45f15f8e0a2 = ?
    947a912ebdd30c7ae7a50b324f85095d = ?
    989c8511a20bc71a9beee31afbc74780 = ?
    9f6199b648a62081f3c140866852748d = ?
    aed4af26c1012517b7eed1978b873061 = ?
    c04faf4dc5de3e3cf84c89b949b7f9b8 = ?
    c5a50225ea7256ade48514fe752ac2c9 = ?
    d7064190381d9ad066dfb708afe483cd = ?
    efef22dd86b6ac6f80b994d2ad7581cd = ?
    f7365b005acfc1c72f1f49a562a807fd = ?

--- Tanji/Habbo/FlashGame.cs
+++ Tanji/Habbo/FlashGame.cs
@@ -92,7 +92,7 @@ namespace Tanji.Habbo
                 names.Add(hash, (name, isUnmatched)); // Duplicate hashes should no longer exist, so allow the exception to be thrown.
             }
 
-            //FindMessageReferences();
+            FindMessageReferences();
             var collisions = new Dictionary<string, int>();
             Messages = new SortedDictionary<string, FlashMessage>();
             foreach (FlashMessage flashMsg in _flashMsgs.Values)
@@ -575,6 +575,7 @@ namespace Tanji.Habbo
             foreach (ASClass @class in abc.Classes)
             {
                 if (@class.Traits.Count != 2 || @class.Instance.Traits.Count != 2) continue;
+                if (@class.Traits[0].Type.Name != "Map" || @class.Traits[1].Type.Name != "Map") continue;
                 if (@class.Traits[0].Kind != TraitKind.Constant || @class.Traits[1].Kind != TraitKind.Constant) continue;
 
                 habboMessagesClass = @class;
